da3f6af72f943652d7e47a92d3a1e0d795c4d4a0
zhangjianxin <zhangjianxin@dhcc.com.cn>
2021-05-14 17:15:09 +0800

    1.基本框架搭建.2.逻辑删除配置。3.新增删除接口

--- service/service_edu/src/main/java/com/atguigu/eduService/config/EduConfig.java
+++ service/service_edu/src/main/java/com/atguigu/eduService/config/EduConfig.java
@@ -1,6 +1,9 @@
 package com.atguigu.eduService.config;
 
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -11,4 +14,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @MapperScan("com.atguigu.eduService.mapper")
 public class EduConfig {
+    /**
+     * 逻辑删除插件
+     */
+    @Bean
+    public ISqlInjector sqlInjector() {
+        return new LogicSqlInjector();
+    }
+
 }
--- service/service_edu/src/main/java/com/atguigu/eduService/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/eduService/controller/EduTeacherController.java
@@ -4,10 +4,7 @@ package com.atguigu.eduService.controller;
 import com.atguigu.eduService.entity.EduTeacher;
 import com.atguigu.eduService.service.EduTeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +26,12 @@ public class EduTeacherController {
     public List<EduTeacher> findAll(){
         List<EduTeacher> list = eduTeacherService.list(null);
         return list;
+    }
+
+    @DeleteMapping("delById/{id}")
+    public boolean delById(@PathVariable("id") String id){
+        boolean b = eduTeacherService.removeById(id);
+        return b;
     }
 }
 
